f527c94533abcb239eb7d879a256d2ad906c6a8b
Taras Shlapak <dalat.deymos@gmail.com>
2021-08-10 21:45:23 +0200

    Updates

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID 6H7BCUg4cvUsRH4gHer6zlggSBIfS9gxTOKiIdS0oBw'
-    }
-});
